7dc672b95c1b93adaceb6199fc417ff71d618f89
overstar <8497715@gmail.com>
2018-10-27 14:56:08 +0500

    moveCharToOut()

--- src/app/scroll-text/scroll-text.component.ts
+++ src/app/scroll-text/scroll-text.component.ts
@@ -9,18 +9,52 @@ export class ScrollTextComponent implements OnInit {
 
   constructor() { }
 
+  inputString: string;
+  outString: string;
+
   ngOnInit() {
+    this.inputString = 'first test string';
+    this.outString = 'test out string';
   }
 
   doKeypress(event: Event) {
-    console.log(event.key);
+    const pressedChar: string = (<KeyboardEvent>event).key;
+
+    if (pressedChar === this.getTargetChar()) {
+      this.moveCharToOut();
+
+    } else {
+      console.log('eroor input char');
+    }
+
+    console.log((<KeyboardEvent>event).key);
+
+
   }
+
+  moveCharToOut() {
+    this.outString  += this.inputString[0];
+    this.inputString = this.inputString.substr(1);
+  }
+
+  getTargetChar() {
+    return this.inputString[0];
+  }
+
   doFocus() {
     console.log('set focus');
+    console.log(this.getTargetChar());
+
+    // TODO Start statistic
+    // TODO Start timer
+    // TODO start input in 3 sec
   }
 
   doLostFocus() {
     console.log('lost focus');
+    // TODO Pause statistic
+    // TODO Pause timer
+    // TODO lock input
   }
 
 }
